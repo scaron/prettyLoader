ed5f8684f7b0400791636e2ab26778b129f21c6a
Stephane Caron <scaron@Stephane-Carons-MacBook-Pro.local>
2010-04-23 10:37:29 -0400

    Bind events only when the loader is showned, added error control on loader path, fixed loader position onscroll

--- js/jquery.prettyLoader.js
+++ js/jquery.prettyLoader.js
@@ -2,32 +2,54 @@
 	Class: prettyLoader
 	Use: A unified solution for AJAX loader
 	Author: Stephane Caron (http://www.no-margin-for-errors.com)
-	Version: 1.0
+	Version: 1.0.1
 ------------------------------------------------------------------------- */
 
 (function($) {
-	$.prettyLoader = {version: '1.0'};
+	$.prettyLoader = {version: '1.0.1'};
 	
 	$.prettyLoader = function(settings) {
 		settings = jQuery.extend({
 			animation_speed: 'fast', /* fast/normal/slow/integer */
 			bind_to_ajax: true, /* true/false */
 			delay: false, /* false OR time in milliseconds (ms) */
-			loader: '/prettyLoader/images/prettyLoader/ajax-loader.gif', /* Path to your loader gif */
+			loader: '/images/prettyLoader/ajax-loader.gif', /* Path to your loader gif */
 			offset_top: 13, /* integer */
 			offset_left: 10 /* integer */
 		}, settings);
 		
 		scrollPos = _getScroll();
-		
-		// Window/Keyboard events
-		$(window).unbind('scroll').scroll(function(){ scrollPos = _getScroll(); });
+
+		imgLoader = new Image();
+		imgLoader.onerror = function(){
+			alert('Preloader image cannot be loaded. Make sure the path is correct in the settings and that the image is reachable.');
+		};
+		imgLoader.src = settings.loader;
 		
 		if(settings.bind_to_ajax)
 			jQuery(document).ajaxStart(function(){ $.prettyLoader.show() }).ajaxStop(function(){ $.prettyLoader.hide() });
+
+		$.prettyLoader.positionLoader = function(e){
+      		e = e ? e : window.event;
+
+			// Set the cursor pos only if the even is returned by the browser.
+			cur_x = (e.clientX) ? e.clientX : cur_x;
+			cur_y = (e.clientY) ? e.clientY : cur_y;
+
+			left_pos = cur_x + settings.offset_left + scrollPos['scrollLeft'];
+			top_pos = cur_y + settings.offset_top + scrollPos['scrollTop'];
+			
+			$('.prettyLoader').css({
+				'top':top_pos,
+				'left':left_pos
+			});
+		}
 	
 		$.prettyLoader.show = function(delay){
 			if($('.prettyLoader').size() > 0) return;
+			
+			// Get the scroll position
+			scrollPos = _getScroll();
 			
 			// Build the loader container
 			$('<div></div>')
@@ -47,30 +69,10 @@
 
 			// Show it!
 			$('.prettyLoader').fadeIn(settings.animation_speed);
-			
-			$(document).bind('click',function(e){
-				e = e ? e : window.event;
-				
-				left_pos = e.clientX + settings.offset_left + scrollPos['scrollLeft'];
-				top_pos = e.clientY + settings.offset_top + scrollPos['scrollTop'];
-				
-				$('.prettyLoader').css({
-					'top':top_pos,
-					'left':left_pos
-				});
-			});
-			
-			$(document).bind('mousemove',function(e){
-				// Get the cursor position
-				e = e ? e : window.event;
-				left_pos = e.clientX + settings.offset_left + scrollPos['scrollLeft'];
-				top_pos = e.clientY + settings.offset_top + scrollPos['scrollTop'];
-				
-				$('.prettyLoader').css({
-					'top':top_pos,
-					'left':left_pos
-				});
-			});
+
+			$(document).bind('click',$.prettyLoader.positionLoader);
+			$(document).bind('mousemove',$.prettyLoader.positionLoader);
+			$(window).scroll(function(){ scrollPos = _getScroll(); $(document).triggerHandler('mousemove'); });
 			
 			delay = (delay) ? delay : settings.delay;
 			
@@ -80,6 +82,10 @@
 		};
 
 		$.prettyLoader.hide = function(){
+			$(document).unbind('click',$.prettyLoader.positionLoader);
+			$(document).unbind('mousemove',$.prettyLoader.positionLoader);
+			$(window).unbind('scroll');
+						
 			$('.prettyLoader').fadeOut(settings.animation_speed,function(){
 				$(this).remove();
 			});
